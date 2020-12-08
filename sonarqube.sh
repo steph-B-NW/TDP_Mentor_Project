7ab0ec7d63e25bae3848f2a7cfcf42d98e506dd6
steph-b-nw <stephen.bowie@nationwide.co.uk>
2020-12-08 16:15:20 +0000

    sonarqube

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,7 +1,5 @@
-#! /bin/bash
-
-echo "sonar-scanner \
-  -Dsonar.projectKey=first-project \
+sonar-scanner \
+  -Dsonar.projectKey=project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=aa0e08500d505b5362ff994999cf892f19bf7ea0"
+  -Dsonar.login=4738ce02ca5fe307ee036d87563756a6011fa3b2
